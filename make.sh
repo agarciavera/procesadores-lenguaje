e1e5c371babf1279788344839946026891148d33
Adrian Garcia-Vera <adrian.garciavera@es.logicalis.com>
2016-10-18 16:18:14 +0200

    Add script make.sh

---
+++ make.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# make.sh
+
+# Executing commands that you need for analyzes the tests files
+java JLex.Main lex.lex
+javac lex.lex.java
+if [ $# = 0 ]
+then
+  echo -e "\nName of file to be analyzed?"
+  read file
+  echo ¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬
+  java Yylex $file
+else
+  echo -e "\nThe file to be analyzed is: $1"
+  echo ¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬
+  java Yylex $1
+fi
